932b0b9c701f7174d8b9c465e45833fa05180a82
angc <angcoder.c@gmail.com>
2026-05-05 17:45:43 -0600

    fix: make api stack runnable from docker compose

--- src/utils/dependencies.py
+++ src/utils/dependencies.py
@@ -1,12 +1,12 @@
 """Dependencias de FastAPI."""
 from fastapi import HTTPException, Depends
-from fastapi.security import HTTPBearer, HTTPAuthCredentials
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from src.utils.jwt import decode_access_token
 
 security = HTTPBearer()
 
 
-async def get_current_user(credentials: HTTPAuthCredentials = Depends(security)) -> dict:
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)) -> dict:
     """Validar JWT y retornar usuario actual."""
     token = credentials.credentials
     payload = decode_access_token(token)
