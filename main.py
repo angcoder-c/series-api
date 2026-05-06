428ea33f7e87111455418a1cc4145e88fa568b3d
angc <angcoder.c@gmail.com>
2026-05-05 18:16:51 -0600

    fix: allow all cors origins in fastapi

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 """Aplicación FastAPI."""
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.auth_router import router as auth_router
 from src.api.series_router import router as series_router
 
 app = FastAPI(title="Series API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(series_router)
